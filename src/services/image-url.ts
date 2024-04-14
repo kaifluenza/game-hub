7690abbabb9aeb2ada188a1444f3a13080e75062
kai34000 <wanrueni.12@gmail.com>
2024-04-14 17:27:09 -0400

    Get optimized images

---
+++ src/services/image-url.ts
@@ -0,0 +1,8 @@
+const getCroppedImageUrl = (url:string) => {
+    const target = 'media/';
+    const index = url.indexOf(target) + target.length; 
+    //refers to index of _ as in "media/_" which is where we want to insert
+
+    return url.slice(0,index) + 'crop/600/400/' + url.slice(index);
+}
+export default getCroppedImageUrl;
