a18ed0524ec3758897832d0d3e198aa4e85c4ce8
kai34000 <wanrueni.12@gmail.com>
2024-05-06 19:49:17 -0400

    Clean up the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,6 +1,7 @@
 import {
   Button,
   HStack,
+  Heading,
   Image,
   List,
   ListItem,
@@ -12,13 +13,12 @@ import getCroppedImageUrl from "../services/image-url";
 import GenreSkeleton from "./GenreSkeleton";
 import GenreContainer from "./GenreContainer";
 
-interface Props{
-  onSelectedGenre:(genre:Genre) => void;
-  selectedGenre:Genre | null;
+interface Props {
+  onSelectedGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
 
-
-const GenreList = ({selectedGenre, onSelectedGenre}:Props) => {
+const GenreList = ({ selectedGenre, onSelectedGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 
@@ -34,6 +34,9 @@ const GenreList = ({selectedGenre, onSelectedGenre}:Props) => {
           </GenreContainer>
         ))}
 
+      <Heading fontSize="2xl" marginBottom={3}>
+        Genres
+      </Heading>
       <List>
         {data.map((genre) => (
           <GenreContainer key={genre.id}>
@@ -42,10 +45,15 @@ const GenreList = ({selectedGenre, onSelectedGenre}:Props) => {
                 <Image
                   boxSize="32px"
                   borderRadius={8}
+                  objectFit="cover" //image will be scaled to fill container while preserving its aspect ratio
                   src={getCroppedImageUrl(genre.image_background)}
                 />
                 <Button
-                  fontWeight={genre.id===selectedGenre?.id? "bold" :"normal" }
+                  textAlign="left"
+                  whiteSpace="normal"
+                  fontWeight={
+                    genre.id === selectedGenre?.id ? "bold" : "normal"
+                  }
                   onClick={() => onSelectedGenre(genre)}
                   fontSize="lg"
                   variant="link"
